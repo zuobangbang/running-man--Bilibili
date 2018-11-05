2a972f99f8e0513c46d01af722435599da0cbebf
zuobangbang <34409711+zuobangbang@users.noreply.github.com>
2018-11-05 17:01:13 +0800

    Update bilibili.py

--- bilibili.py
+++ bilibili.py
@@ -16,7 +16,7 @@ def getTime():
     print(time.time()*1000)
 #getTime()
 #url='https://api.bilibili.com/x/v2/reply?callback=jQuery17202479861083706283_1540867066061&jsonp=jsonp&pn=3&type=1&oid=18089528&sort=0&_='+str(int(time.time()*1000))
-
+#获得整个视频集的相关信息
 def get_video_inf(url):
     headers = {
         'Cookie': 'LIVE_BUVID=AUTO6915375856445563; stardustvideo=1; buvid3=EEB30FF5-70B4-4E16-B15A-94FE3F7AF07F163007infoc; rpdid=kmpsximoqmdoskmsqsxqw; finger=edc6ecda; CURRENT_FNVAL=16; sid=4ih3nm1u; fts=1540866975; UM_distinctid=166c2db295180-082aa06c2afa6d-47e1039-100200-166c2db2952110',
@@ -40,20 +40,7 @@ def get_video_inf(url):
     video_inf['分享']=html['share']
     video_inf['观看次数']=html['view']
     return video_inf
-def get_pages(url):
-    headers={
-        'Cookie': 'LIVE_BUVID=AUTO6915375856445563; stardustvideo=1; buvid3=EEB30FF5-70B4-4E16-B15A-94FE3F7AF07F163007infoc; rpdid=kmpsximoqmdoskmsqsxqw; _uuid=0E9564E0-EFCD-80A6-819D-B0BF6986056427195infoc; finger=edc6ecda; CURRENT_FNVAL=16; sid=4ih3nm1u; fts=1540866975; UM_distinctid=166c2db295180-082aa06c2afa6d-47e1039-100200-166c2db2952110',
-        'Host': 'www.bilibili.com',
-        'Referer': 'https: // space.bilibili.com / 27580511 / ',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-    }
-    html=requests.get(url,headers=headers).text
-    print(html)
-    print('共376页'in html)
-    soup=BeautifulSoup(html,'lxml')
-    pages=soup.find('div',class_='bb-comment')
-    print(pages)
+#获取评论信息
 def get_comment(url):
     headers={
         'Cookie': 'LIVE_BUVID=AUTO6915375856445563; stardustvideo=1; buvid3=EEB30FF5-70B4-4E16-B15A-94FE3F7AF07F163007infoc; rpdid=kmpsximoqmdoskmsqsxqw; finger=edc6ecda; CURRENT_FNVAL=16; sid=4ih3nm1u; fts=1540866975; UM_distinctid=166c2db295180-082aa06c2afa6d-47e1039-100200-166c2db2952110',
@@ -83,36 +70,8 @@ if __name__=='__main__':
     video_url='https://api.bilibili.com/x/web-interface/view?aid=18089528'
     video=get_video_inf(video_url)
     #print(video)
-    page_url='https://www.bilibili.com/video/av12551207'
-    #pages(page_url)
     pages=183
-    url = 'https://api.bilibili.com/x/v2/reply?callback=jQuery17207334597019735183_1540982500519&jsonp=jsonp&pn=1&type=1&oid=12551207&sort=0&_=' + str(
-        int(time.time() * 1000))
-    headers = {
-        'Cookie': 'LIVE_BUVID=AUTO6915375856445563; stardustvideo=1; buvid3=EEB30FF5-70B4-4E16-B15A-94FE3F7AF07F163007infoc; rpdid=kmpsximoqmdoskmsqsxqw; finger=edc6ecda; CURRENT_FNVAL=16; sid=4ih3nm1u; fts=1540866975; UM_distinctid=166c2db295180-082aa06c2afa6d-47e1039-100200-166c2db2952110',
-        'Host': 'api.bilibili.com',
-        'Referer': 'https://www.bilibili.com/video/av18089528',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-    }
-    df = pandas.DataFrame()
-    html = requests.get(url, headers=headers).text
-    html = (html.split('(', 1))[1][:-1]
-    html = json.loads(html)
-    reps = html['data']['replies']
-    for rep in reps:
-        inf = {}
-        inf['评论'] = rep['content']['message']
-        inf['time'] = rep['ctime']
-        inf['floor'] = rep['floor']
-        inf['点赞'] = rep['like']
-        inf['性别'] = rep['member']['sex']
-        inf['name'] = rep['member']['uname']
-        inf['等级'] = rep['member']['level_info']['current_level']
-        inf['回复'] = rep['rcount']
-        df = df.append(inf, ignore_index=True)
-        #print(inf)
-    df.to_csv('runningman2017.csv', index=False, mode='a', header=False,encoding='utf-8',columns=['评论','time','floor','点赞','性别','name','等级','回复'])
-    for page in range(2,pages):
+    for page in range(1,pages):
         print('开始抓取第%d页'%page)
         url = 'https://api.bilibili.com/x/v2/reply?callback=jQuery17207334597019735183_1540982500519&jsonp=jsonp&pn='+str(page)+'&type=1&oid=12551207&sort=0&_=' + str(
         int(time.time() * 1000))
